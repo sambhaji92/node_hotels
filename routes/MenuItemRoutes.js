907f40b694cd21c64df7bfd460a56e1ac938d4ac
ranjeet sambhaji <sambhaji.ranjeet83@gmail.com>
2026-04-21 16:52:32 +0530

    next version of project

--- routes/MenuItemRoutes.js
+++ routes/MenuItemRoutes.js
@@ -42,6 +42,5 @@ res.status(200).json(data);
 //   } catch(err){
 //     console.log(err);
 //     res.status(500).json({error:'internal saver error'});
-//   }
-//  })
+//    
 module.exports=router;
